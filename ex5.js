29539cad72e0bf94d145f97722fbc588c09a77d8
Lisa Ostman <peolina@live.com.au>
2021-09-10 11:21:21 +1000

    fixed some instructions

--- ex5.js
+++ ex5.js
@@ -2,7 +2,8 @@
 
 // 1. That logs “The current date and time is “ - the current date and time  (Anonymous function)
 // 2. That prints “I’m ready” after 5 seconds. (Anonymous function)
-//  Watch Javascript functions : https://www.youtube.com/watch?v=Ho2h5BWSHDM
-// 3. To convert today’s temperature from Celsius to Fahrenheit 
-        // a) Celsius to Fahrenheit: (°C × 9/5) + 32 = °F
-        // b) Fahrenheit to Celsius: (°F − 32) x 5/9 = °C
+// hint; you may wish to research the Date object to get the current date!
+
+// 3. Create a function to convert today’s temperature from Celsius to Fahrenheit
+// a) Celsius to Fahrenheit: (°C × 9/5) + 32 = °F
+// b) Fahrenheit to Celsius: (°F − 32) x 5/9 = °C
